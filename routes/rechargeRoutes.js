b24b7572c8ed7bbdce3bf626d57af00c1df6be6a
AchaUmbaPretre <achandambi@gmail.com>
2024-05-27 16:20:47 +0100

    commit

--- routes/rechargeRoutes.js
+++ routes/rechargeRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { getRecharge, postRecharge, deleteRecharge, getRechargeClientTotal } = require("../controllers/rechargeController");
+const { getRecharge, postRecharge, deleteRecharge, getRechargeClientTotal, getRechargeClientOne } = require("../controllers/rechargeController");
 const router = express.Router();
 
 router.get('/rechargerClient', getRechargeClientTotal)
-router.get('/rechargerNumero', getRechargeClientTotal)
+router.get('/rechargerClientOne', getRechargeClientOne)
 router.get('/', getRecharge)
 router.post('/', postRecharge)
 router.delete('/', deleteRecharge)
