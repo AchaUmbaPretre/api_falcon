b180f524bd81da20a9391d76b5419906e97a34cb
AchaUmbaPretre <achandambi@gmail.com>
2024-05-28 13:41:48 +0100

    commit

--- routes/traceurRoutes.js
+++ routes/traceurRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getTraceur, postTraceur, getTraceurEtat, getModelTraceur, getTraceurCount } = require("../controllers/traceurController");
+const { getTraceur, postTraceur, getTraceurEtat, getModelTraceur, getTraceurCount, getTraceurHistorique } = require("../controllers/traceurController");
 const router = express.Router();
 
 router.get('/count', getTraceurCount)
 router.get('/', getTraceur)
+router.get('/historique', getTraceurHistorique)
 router.get('/traceur_etat', getTraceurEtat)
 router.get('/traceur_model', getModelTraceur)
 router.post('/', postTraceur)
